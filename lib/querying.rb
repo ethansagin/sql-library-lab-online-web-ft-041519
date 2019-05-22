c0147da11dc72e162b61bb11fcd0f54df5be68e1
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 00:41:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,5 +19,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT Characters.name FROM Characters LEFT JOIN Character_books"
+  "SELECT Characters.name COUNT(Character_books.character_id) FROM Characters LEFT JOIN Character_books"
 end
