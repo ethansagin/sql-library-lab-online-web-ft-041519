02cd402021b1ff9de7ce1fc74af7f3f81fab392f
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 01:32:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title, Characters.species FROM series JOIN Characters ON Series.id = Characters.series_id WHERE Characters.species = 'human';"
+  "SELECT Series.title, Characters.species FROM series JOIN Characters ON Series.id = Characters.series_id WHERE Characters.species = 'human' GROUP BY Series.title;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
