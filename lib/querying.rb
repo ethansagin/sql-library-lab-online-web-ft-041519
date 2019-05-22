6de8200ca9f161fa36dbecccbe4bf85936e0012e
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 01:09:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year ASC;"
@@ -17,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  dir = "SELECT Series.title, Characters.name, Characters.species, Characters.series_id FROM series JOIN Characters ON Series.id = Characters.series_id;"
+  dir = "SELECT Series.title, Characters.name FROM series JOIN Characters ON Series.id = Characters.series_id WHERE Characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
